8f8e127f66625eb916f73a89965457f9b157803f
Ray Luo <luofeim@kehwa.net>
2016-03-01 23:25:31 +0800

    8.branch.finished web all

---
+++ src/main/java/org/rooinaction/coursemanager/web/CourseController.java
@@ -0,0 +1,15 @@
+package org.rooinaction.coursemanager.web;
+import org.rooinaction.coursemanager.model.Course;
+import org.springframework.roo.addon.web.mvc.controller.annotations.scaffold.RooWebScaffold;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.roo.addon.web.mvc.controller.annotations.finder.RooWebFinder;
+import org.springframework.roo.addon.web.mvc.controller.annotations.json.RooWebJson;
+
+@RequestMapping("/courses")
+@Controller
+@RooWebScaffold(path = "courses", formBackingObject = Course.class)
+@RooWebFinder
+@RooWebJson(jsonObject = Course.class)
+public class CourseController {
+}
