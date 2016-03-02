749d92c52bcdbf64549c1e45accdcfbaf18b98c6
Ray Luo <luofeim@kehwa.net>
2016-03-02 10:12:25 +0800

    2-1.3.entity

---
+++ src/main/java/org/rooinaction/coursemanager/model/Course.java
@@ -0,0 +1,10 @@
+package org.rooinaction.coursemanager.model;
+import org.springframework.roo.addon.javabean.annotations.RooJavaBean;
+import org.springframework.roo.addon.javabean.annotations.RooToString;
+import org.springframework.roo.addon.jpa.annotations.entity.RooJpaEntity;
+
+@RooJavaBean
+@RooToString
+@RooJpaEntity
+public class Course {
+}
