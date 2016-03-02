f297e2e41fec6f3fb673cb3e76b82a01e27227ed
Ray Luo <luofeim@kehwa.net>
2016-03-02 11:57:34 +0800

    2-1.5.1.more integration test

---
+++ src/test/java/org/rooinaction/coursemanager/model/OfferingIntegrationTest.java
@@ -0,0 +1,11 @@
+package org.rooinaction.coursemanager.model;
+import org.junit.Test;
+import org.springframework.roo.addon.test.annotations.RooIntegrationTest;
+
+@RooIntegrationTest(entity = Offering.class)
+public class OfferingIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
