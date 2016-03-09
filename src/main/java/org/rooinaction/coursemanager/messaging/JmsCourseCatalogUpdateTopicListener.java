b6d366da08797dd152837be49650be78efa4acf9
Ray Luo <luofeim@kehwa.net>
2016-03-09 11:57:26 +0800

    f.jms

---
+++ src/main/java/org/rooinaction/coursemanager/messaging/JmsCourseCatalogUpdateTopicListener.java
@@ -0,0 +1,8 @@
+package org.rooinaction.coursemanager.messaging;
+
+public class JmsCourseCatalogUpdateTopicListener {
+
+    public void onMessage(Object message) {
+        System.out.println("JMS message received: " + message);
+    }
+}
--- src/main/java/org/rooinaction/coursemanager/web/CourseController.java
+++ src/main/java/org/rooinaction/coursemanager/web/CourseController.java
@@ -1,10 +1,17 @@
 package org.rooinaction.coursemanager.web;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.rooinaction.coursemanager.model.Course;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.roo.addon.web.mvc.controller.annotations.finder.RooWebFinder;
+import org.springframework.roo.addon.web.mvc.controller.annotations.json.RooWebJson;
 import org.springframework.roo.addon.web.mvc.controller.annotations.scaffold.RooWebScaffold;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.roo.addon.web.mvc.controller.annotations.finder.RooWebFinder;
-import org.springframework.roo.addon.web.mvc.controller.annotations.json.RooWebJson;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequestMapping("/courses")
 @Controller
@@ -12,4 +19,18 @@ import org.springframework.roo.addon.web.mvc.controller.annotations.json.RooWebJ
 @RooWebFinder
 @RooWebJson(jsonObject = Course.class)
 public class CourseController {
+	@Autowired
+	private transient JmsTemplate jmsTemplate;
+
+	public void sendMessage(Object messageObject) {
+		jmsTemplate.convertAndSend(messageObject);
+	}
+
+	@RequestMapping(value = "/sendMessage", method = RequestMethod.GET, produces = "text/html")
+	@ResponseBody
+	public String sendMessage(HttpServletRequest httpServletRequest) {
+		sendMessage("send hello world......");
+		return "message was sent......";
+	}
+
 }
